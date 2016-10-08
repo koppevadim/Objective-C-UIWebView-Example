f26da85b49a58dea87c6787fe9783bde91dec673
Vadim Koppe <vadimkoppe@MacBook-Vadim.local>
2016-10-08 14:42:29 +0300

    Initial Commit

---
+++ UIWebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIWebView
+//
+//  Created by Vadim Koppe on 08.10.16.
+//  Copyright © 2016 Vadim Koppe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIWebView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UIWebView
+//
+//  Created by Vadim Koppe on 08.10.16.
+//  Copyright © 2016 Vadim Koppe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
